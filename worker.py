64dc41549d63d6917569c40dfa827200e4438fcf
anton0909 <anton0909@naver.com>
2024-03-16 01:20:45 +0900

    On branch master Your branch is behind 'origin/master' by 1 commit, and can be fast-forwarded.   (use "git pull" to update your local branch)
    Changes to be committed:
    	new file:   .gitignore
    	modified:   worker.py

--- worker.py
+++ worker.py
@@ -31,25 +31,31 @@ class Worker(QThread):
 
     def get_boxes(self, frame, result):
         bboxes = []
+        max_area = 0
+        center_bbox = None
         for boxes in result.boxes:
             x1, y1, x2, y2, score, classes = boxes.data.squeeze().cpu().numpy()
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            area = (x2 - x1) * (y2 - y1)
             bboxes.append((x1, y1, x2, y2))
-        return bboxes
+            if area > max_area:
+                max_area = area
+                center_bbox = ((x1 + x2) // 2, (y1 + y2) // 2)
+        if center_bbox is None:
+            center_bbox = (0, 0)
+        return bboxes, center_bbox
 
     def run(self):
         while True:
-
             sct = mss()
-
-            screenshot = sct.grab(sct.monitors[0])  # 화면 캡처
+            screenshot = sct.grab(sct.monitors[0])
             image = np.array(screenshot)
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             result = self.predict(image)
-            bbox = self.get_boxes(image, result)
+            bbox, center = self.get_boxes(image, result)
+            if center[0] != 0 and center[1] != 0:
+                pag.moveTo(center[0], center[1])
             self.timeout.emit(bbox)
-
-        #cv2.imshow('MediaPipe Pose', image)
             if keyboard.is_pressed('q'):
                 break
         
